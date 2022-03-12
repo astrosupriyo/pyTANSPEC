2373f7588a1826718aef208f56122e3c953abf23
SupriyoGhosh <sbuphy2010@gmail.com>
2022-03-12 22:41:59 +0530

    entry_points name changed

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ setup(name='pyTANSPEC',
       license='GPLv3+',
       packages=['pyTANSPEC','pyTANSPEC.libs'],
       entry_points = {
-          'console_scripts': ['redspecXD=pyTANSPEC.reduce_specXD:main'],
+          'console_scripts': ['pyxdspec=pyTANSPEC.reduce_specXD:main'],
       },
       install_requires=[
           'numpy',
